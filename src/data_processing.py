e1cc593e8397538e1f8b7ebbfeb9a7291316b1e3
raisssu <rizoan789@gmail.com>
2026-03-24 03:38:15 -0400

    chore: init + eda + initial preprocessing

---
+++ src/data_processing.py
@@ -0,0 +1,20 @@
+from scipy.signal import butter, lfilter
+import scipy.io
+import numpy as np
+import wfdb
+import tqdm
+
+def bandpass_filter (signal, lowcut, highcut, sampling_rate, order = 2):
+    
+    nyquist = 0.5 * sampling_rate
+    low = lowcut / nyquist
+    high = highcut / nyquist
+    b, a = butter(order, [low, high], btype = "band")
+    y = lfilter(b, a, signal)
+    return y
+
+def hello():
+    return "Gurt"
+def derivative_filter(ecg_signal, fs = 100):
+    kernel = np.array([-1, -2, 0, 1, 2]) * (1/8)
+    return np.convolve(ecg_signal, kernel)
